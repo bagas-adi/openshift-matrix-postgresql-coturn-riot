d015b21a1c5493f301495aa189ad62b20482f76b
Administrator <admin@example.com>
2020-04-07 17:31:55 +0700

    updated

--- matrix-synapse-docker/scripts/alter_config.sh
+++ matrix-synapse-docker/scripts/alter_config.sh
@@ -23,9 +23,11 @@ sed -i "s|- webclient  # The bundled|# - webclient  # The bundled|g" $DATA_DIR/$
 # URL preview
 sed -i "s|url_preview_enabled: False|url_preview_enabled: True|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# url_preview_ip_range_blacklist:|url_preview_ip_range_blacklist:|g" $DATA_DIR/$CONFIG_FILE
+sed -i "s|#enable_registration: false|enable_registration: true|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# - '127.0.0.0/8'| - '127.0.0.0/8'|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# - '10.0.0.0/8'| - '10.0.0.0/8'|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# - '172.16.0.0/12'| - '172.16.0.0/12'|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# - '192.168.0.0/16'| - '192.168.0.0/16'|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# - '100.64.0.0/10'| - '100.64.0.0/10'|g" $DATA_DIR/$CONFIG_FILE
 sed -i "s|# - '169.254.0.0/16'| - '169.254.0.0/16'|g" $DATA_DIR/$CONFIG_FILE
+sed -i "s|'127.0.0.1'|'0.0.0.0'|g"  $DATA_DIR/$CONFIG_FILE
